4e20d04f5773c667d4084fe8ae9e34dc95e99222
SergioScata <sergiopio395@gmail.com>
2024-01-22 09:20:16 +0100

    Initial commit

---
+++ src/test/java/org/lessons/blog/ricette/BlogRicetteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.blog.ricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRicetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
